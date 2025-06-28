0e4cfd422154aed2eba80e8db76880c24062be14
BytelyPlay <100842216+Killerof6@users.noreply.github.com>
2025-06-28 11:30:41 +0200

    Just wanted to commit again while i'm cleaning up the code so i don't lose my progress

--- src/main/java/org/hyperoil/playifkillers/Listeners/Commands.java
+++ src/main/java/org/hyperoil/playifkillers/Listeners/Commands.java
@@ -8,14 +8,26 @@ import net.minestom.server.instance.Chunk;
 import net.minestom.server.instance.InstanceContainer;
 import net.minestom.server.instance.block.Block;
 import org.hyperoil.playifkillers.Main;
+import org.hyperoil.playifkillers.Utils.Box;
+import org.jspecify.annotations.Nullable;
 
 import java.util.UUID;
+import java.util.concurrent.ExecutorService;
 
 public class Commands {
-    public static void onPlayerCommandEvent(PlayerCommandEvent event) {
+    private final ExecutorService service;
+    private final InstanceContainer container;
+
+    public Commands(ExecutorService executor, InstanceContainer contain) {
+        service = executor;
+        container = contain;
+    }
+
+    public void onPlayerCommandEvent(PlayerCommandEvent event) {
         parseAdminCommands(event);
     }
-    private static void parseAdminCommands(PlayerCommandEvent event) {
+
+    private void parseAdminCommands(PlayerCommandEvent event) {
         final String command = event.getCommand();
         final Player p = event.getPlayer();
         UUID playerUUID = p.getUuid();
@@ -34,7 +46,7 @@ public class Commands {
                 }
                 int[] coordinatesSplit = new int[6];
                 int coordSplitCounter = 0;
-                for (int i = 1; i<7; i++) {
+                for (int i = 1; i < 7; i++) {
                     String on = commandSplit[i];
                     try {
                         coordinatesSplit[coordSplitCounter] = Integer.parseInt(on);
@@ -44,23 +56,13 @@ public class Commands {
                         return;
                     }
                 }
-                Main.executorService.submit(() -> {
-                    for (int x = coordinatesSplit[0]; x <= coordinatesSplit[3]; x++) {
-                        for (int y = coordinatesSplit[1]; y <= coordinatesSplit[4]; y++) {
-                            for (int z = coordinatesSplit[2]; z <= coordinatesSplit[5]; z++) {
-                                InstanceContainer world = Main.overWorld;
-                                Chunk chunk = world.getChunk(x >> 4, z >> 4);
-                                if (chunk != null && chunk.isLoaded()) {
-                                    world.setBlock(x, y, z, block);
-                                }
-                                if (block != Block.AIR) {
-                                    Main.blocksSaved.put(new BlockVec(x, y, z), block);
-                                } else {
-                                    Main.blocksSaved.remove(new BlockVec(x, y, z));
-                                }
-                            }
-                        }
-                    }
+                service.submit(() -> {
+                    fillCommandHandler(new Box(coordinatesSplit[0],
+                            coordinatesSplit[1],
+                            coordinatesSplit[2],
+                            coordinatesSplit[3],
+                            coordinatesSplit[4],
+                            coordinatesSplit[5]), block);
                 });
             } else {
                 p.sendMessage("Please supply at least 7 arguments");
@@ -68,5 +70,36 @@ public class Commands {
         } else {
             p.sendMessage("Unknown Command.");
         }
+    }
+
+    private void fillCommandHandler(Box box, Block block) {
+        InstanceContainer container = Main.overWorld;
+        for (BlockVec vec : box.getAllBlocks(Main.overWorld)) {
+            container.setBlock(vec, block);
+        }
+    }
+
+    private Box getBoxFromCommand(Player p, String[] commandSplit) {
+        if (commandSplit.length == 7) {
+            int[] coordinatesSplit = new int[6];
+            int coordSplitCounter = 0;
+            for (int i = 1; i < 7; i++) {
+                String on = commandSplit[i];
+                try {
+                    coordinatesSplit[coordSplitCounter] = Integer.parseInt(on);
+                    coordSplitCounter++;
+                } catch (NumberFormatException e) {
+                    p.sendMessage("Invalid number. on argument " + i + 1);
+                    return null;
+                }
+            }
+            return new Box(coordinatesSplit[0],
+                    coordinatesSplit[1],
+                    coordinatesSplit[2],
+                    coordinatesSplit[3],
+                    coordinatesSplit[4],
+                    coordinatesSplit[5]);
+        }
+        return null;
     }
 }
---
+++ src/main/java/org/hyperoil/playifkillers/Utils/Box.java
@@ -0,0 +1,39 @@
+package org.hyperoil.playifkillers.Utils;
+
+import net.minestom.server.coordinate.BlockVec;
+import net.minestom.server.coordinate.Point;
+import net.minestom.server.coordinate.Pos;
+import net.minestom.server.instance.InstanceContainer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Box {
+    private final int x1;
+    private final int y1;
+    private final int z1;
+    private final int x2;
+    private final int y2;
+    private final int z2;
+
+    public Box(int givenX1, int givenY1, int givenZ1, int givenX2, int givenY2, int givenZ2) {
+        x1 = givenX1;
+        y1 = givenY1;
+        z1 = givenZ1;
+        x2 = givenX2;
+        y2 = givenY2;
+        z2 = givenZ2;
+    }
+
+    public List<BlockVec> getAllBlocks(InstanceContainer container) {
+        ArrayList<BlockVec> blocks = new ArrayList<>();
+        for (int x = x1; x < x2; x++) {
+            for (int y = y1; y < y2; y++) {
+                for (int z = z1; z < z2; z++) {
+                    blocks.add(new BlockVec(x, y, z));
+                }
+            }
+        }
+        return List.copyOf(blocks);
+    }
+}
